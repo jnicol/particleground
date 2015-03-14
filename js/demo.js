3c5b898c9bf64f87c5490402419d59f01a332143
Jonathan Nicol <jonathan@jonathannicol.com>
2015-03-14 22:16:18 +1100

    Updated gh-pages demo for 1.1.0

--- js/demo.js
+++ js/demo.js
@@ -3,6 +3,18 @@
  * @author Jonathan Nicol - @mrjnicol
  */
 
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
 $(document).ready(function() {
   $('#particles').particleground({
     dotColor: '#5cbdaa',
@@ -12,3 +24,4 @@ $(document).ready(function() {
     'margin-top': -($('.intro').height() / 2)
   });
 });
+*/
